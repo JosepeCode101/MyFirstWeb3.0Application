0c32a7eb3f329fb6085dffe258434bf6135e1494
JosepeCode <72314772+JosepeCode101@users.noreply.github.com>
2022-02-11 09:33:50 +0100

    Fetch our SmartContract with TRX context

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -6,7 +6,7 @@ import { contractABI, contractAddress } from '../utils/constants';
 // 
 export const TransactionContext = React.createContext();
 
-// Ethereum window object
+// Ethereum window object estamos desestruccturando el objeto ETH de window.ethereum
 const { ethereum } = window;
 
 // ACCEDER A LA BLOCKCHAIN
@@ -20,5 +20,26 @@ const getEthereumContract = () => {
     const provider = new ethers.providers.Web3Provider(ethereum);
 
     const signer = provider.getSigner();
+
+
+    // generamos el transaction contract con la direccion ABI y firmante que nos servirarn para recuperar nuestro SC
+    const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+
+    // imprimir los objetos por consola
+    console.log({
+        provider,
+        signer,
+        transactionContract
+    });
+
+}
+
+export const TransactionProvider = ({ children }) => {
+    return (
+        <TransactionProvider value={{ value: 'test' }}>
+            {children}
+        </TransactionProvider>
+    )
 
 }
