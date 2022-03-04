00686eeb2e174ef5f8c759d5178c3df347d4854a
JosepeCode <72314772+JosepeCode101@users.noreply.github.com>
2022-03-04 11:56:12 +0100

    Gifs attached to dummyData Trx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -9,8 +9,14 @@ import { TransactionContext } from '../context/TransactionContext';
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
 
+import useFetch from '../hooks/useFetch';
+
 
 const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+    
+    const gifUrl = useFetch({ keyword })
+    
+    
     return (
             <div className='bg-[#0A2041] m-4 flex flex-1
               2xl-min-w-[450px]
@@ -34,11 +40,17 @@ const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, a
                                 <p className='tect-white text-base'> Message: {message}</p>
                             </>
                         )}
+                        
+                    </div>
+                    <img 
+                            src={gifUrl || url}
+                            alt='gif'
+                            className='w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover'
+                        />
 
                         <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl'> 
                             <p className='text-[#37c7da] font-bold'>{timestamp}</p>
                         </div>
-                    </div>
 
                 </div>
  
--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,5 +1,36 @@
+// imports de react
 import { useEffect, useState } from "react";
 
+// llamada a la llave de la API
 const API_KEY = import.meta.env.VITE_GIPHY_API;
 
-const 
+// Componente 
+const useFetch = ({ keyword }) => {
+
+    const [gifUrl, setGifUrl] = useState('');
+
+    const fetchGifs = async () =>  {
+        try {
+            // conectarse con la API
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`);
+
+            const { data } = await response.json();
+
+            setGifUrl(data[0]?.images?.downsized_medium?.url)
+
+        } catch (error) {
+            setGifUrl('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284')
+        }
+    }
+
+    useEffect(() => {
+
+        if(keyword) fetchGifs();
+
+    }, [keyword])
+
+    return gifUrl;
+
+}
+
+export default useFetch;
